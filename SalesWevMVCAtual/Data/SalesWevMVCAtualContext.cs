e14cd020552c25d3573b7df6ce1127109179bf13
fioravante-joao <joao.fioravante@hotmail.com>
2020-09-08 15:40:04 -0300

    crud scaffold

---
+++ SalesWevMVCAtual/Data/SalesWevMVCAtualContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWevMVCAtual.Models;
+
+namespace SalesWevMVCAtual.Data
+{
+    public class SalesWevMVCAtualContext : DbContext
+    {
+        public SalesWevMVCAtualContext (DbContextOptions<SalesWevMVCAtualContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWevMVCAtual.Models.Department> Department { get; set; }
+    }
+}
--- SalesWevMVCAtual/Startup.cs
+++ SalesWevMVCAtual/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWevMVCAtual.Data;
 
 namespace SalesWevMVCAtual
 {
@@ -33,6 +35,9 @@ namespace SalesWevMVCAtual
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWevMVCAtualContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWevMVCAtualContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
